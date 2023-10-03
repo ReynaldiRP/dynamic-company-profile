1ab1dfe5eefb2a7f83bb1bedb602e98ae960bd91
ReynaldiRP1 <rizkypratama471@gmail.com>
2023-10-03 11:04:32 +0700

    [style] add style to the form dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     <script src="https://unpkg.com/@popperjs/core@2"></script>
 
     <!-- Main Styling -->
+    @vite('resources/css/app.css')
     <link href="{{ asset('assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5') }}" rel="stylesheet" />
 </head>
 
@@ -44,6 +45,9 @@
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="{{ asset('assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
--- resources/views/layouts/navbars/nav.blade.php
+++ resources/views/layouts/navbars/nav.blade.php
@@ -30,10 +30,6 @@
                     </div>
                 </div>
                 <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
-                    <!-- online builder btn  -->
-                    <!-- <li class="flex items-center">
-                                <a class="inline-block px-8 py-2 mb-0 mr-4 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro border-fuchsia-500 ease-soft-in text-xs hover:scale-102 active:shadow-soft-xs text-fuchsia-500 hover:border-fuchsia-500 active:bg-fuchsia-500 active:hover:text-fuchsia-500 hover:text-fuchsia-500 tracking-tight-soft hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
-                              </li> -->
                     <li class="flex items-center">
                         <a href="../pages/sign-in.html"
                             class="block px-0 py-2 font-semibold transition-all ease-nav-brand text-sm text-slate-500">
@@ -56,7 +52,6 @@
                     <li class="flex items-center px-4">
                         <a href="javascript:;" class="p-0 transition-all text-sm ease-nav-brand text-slate-500">
                             <i fixed-plugin-button-nav class="cursor-pointer fa fa-cog"></i>
-                            <!-- fixed-plugin-button-nav  -->
                         </a>
                     </li>
 
